ee37375bc1f73e4ee9228b25802e1612aa1a4787
Dima Myronenko <dimyr7@Skynet.local>
2016-11-30 11:29:54 -0600

    Can add as many classes as possible

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 var xml2js =require('xml2js')
 var request = require('request')
 
-var math402 = "http://courses.illinois.edu/cisapp/explorer/schedule/2017/Spring/MATH/402.xml?mode=cascade"
-var math441 = "http://courses.illinois.edu/cisapp/explorer/schedule/2017/Spring/MATH/441.xml?mode=cascade"
+var prefix = "http://courses.illinois.edu/cisapp/explorer/schedule/2017/Spring/";
 
 var twilio = require('twilio')
 data= require('./info.js');
@@ -11,44 +10,47 @@ var authToken = data.authToken;
 var twilioPhone = data.twilioPhone;
 var myPhone = data.myPhone;
 client = new twilio.RestClient(accountSid, authToken)
-request(math441, function(error, response, body){
-	if(!error && response.statusCode == 200){
-		xml2js.parseString(body, function(err, result){
-			var sections = (result["ns2:course"].detailedSections[0].detailedSection);
-			var found = false;
-			for(var i = 0; i < sections.length; i++){
-				var sect = sections[i]
-				var statusSect = sect.enrollmentStatus[0]
-				if(statusSect != 'Closed'){
-					found = true;
-					console.log("Open: " + sect['$'].id);
-					client.messages.create({
-						body: sect['$'].id,
-						to: myPhone,
-						from: twilioPhone
-					}, function(err, message){
-						console.log(message.sid);
-					});
+function checkAvailability(dept, num){
+	var url = prefix + dept + "/" + num + ".xml?mode=cascade";
+	request(url, function(error, response, body){
+		if(!error && response.statusCode == 200){
+			xml2js.parseString(body, function(err, result){
+				var sections = (result["ns2:course"].detailedSections[0].detailedSection);
+				var found = false;
+				for(var i = 0; i < sections.length; i++){
+					var sect = sections[i]
+						var statusSect = sect.enrollmentStatus[0]
+						if(statusSect != 'Closed'){
+							found = true;
+							var message = dept + num + " - " +  sect['$'].id;
+							console.log(message)
+							client.messages.create({
+								body: message,
+								to: myPhone,
+								from: twilioPhone
+							}, function(err, message){
+								console.log(message.sid);
+							});
+						}
 				}
-			}
-			if(!found){
-				client.messages.create({
-					body: "Still no classes found",
-					to: myPhone,
-					from: twilioPhone
-				}, function(err, message){
-					console.log(message.sid);
-				});
-			}
-		});
-	}
-	else{
-		client.messages.create({
-			body: "Something went wrong with ClassNotifier",
-			to: myPhone,
-			from: twilioPhone
-		}, function(err, message){
-			console.log(message.sid);
-		});
-	}
-});
+				if(!found){
+					var message = dept + num + " - still no classes found";
+					console.log(message);
+				}
+			});
+		}
+		else{
+			client.messages.create({
+				body: "Something went wrong with ClassNotifier",
+				to: myPhone,
+				from: twilioPhone
+			}, function(err, message){
+				console.log(message.sid);
+			});
+		}
+	});
+}
+
+checkAvailability("MATH", "441")
+checkAvailability("MATH", "403")
+
